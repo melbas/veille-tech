38b9aec389fabd971aaa7feb39f14970bd09beea
Abdoulaye <melbas@users.noreply.github.com>
2026-09-19 05:07:42 +0200

    ci: deploy GitHub Pages (build statique + basePath)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ export default {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: "/veille-tech",
   webpack: (config) => {
     config.resolve.alias["@"] = new URL("./", import.meta.url).pathname;
     return config;
